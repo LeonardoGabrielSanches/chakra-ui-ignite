7b19216df97c34c98af576ff45c1994b6d6bcb3b
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-06-16 14:52:52 -0300

    Formulario de sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from '../components/Form/Input'
 
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+  const { register, handleSubmit } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = (data) => {
+    console.log(data);
+  }
+
   return (
     <Flex
       w="100vw"
@@ -17,11 +29,12 @@ export default function SignIn() {
         p="8"
         borderRadius="8"
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
+          <Input name="email" label="E-mail" type="email" {...register('email')} />
 
-          <Input name="password" label="Senha" type="password" />
+          <Input name="password" label="Senha" type="password" {...register('password')} />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
