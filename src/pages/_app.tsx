4bb75b2f93055156b4abf41cb93e81416f2a32d8
Leonardo Sanches <sleonardogabriel@gmail.com>
2021-09-23 13:24:19 -0300

    React Query com listagem de usuários.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { theme } from '../styles/theme';
+
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { makeServer } from '../services/mirage';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   )
 }
